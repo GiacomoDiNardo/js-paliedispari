ddcf619a7cd3217b21a2744b3785033516c3a540
GiacomoDiNardo <vest_95@hotmail.it>
2022-05-26 17:05:44 +0200

    add funtion

--- palindromi/main.js
+++ palindromi/main.js
@@ -1,29 +1,31 @@
 const parolaUtente = prompt("Inserisci una parola");
-const parolaDaSinistra = [];
-const parolaDaDestra = [];
 
-const parolaScomposta = parolaUtente.split("");
 
-for(let i = 0; i < parolaScomposta.length; i++) {
 
-    parolaDaSinistra.push(parolaScomposta[i]);
-    console.log(parolaDaSinistra);
-}
+function palindroma (parola) {
+    const parolaRegular = [];
+    const parolaReverse = [];
 
-for(let i = parolaScomposta.length; i >= 0; i--) {
-
-    parolaDaDestra.push(parolaScomposta[i]);
-    console.log(parolaDaDestra);
+    const parolaScomposta = parola.split("");
     
+    for(let i = 0; i < parolaScomposta.length; i++) {
+
+        parolaRegular.push(parolaScomposta[i]);
+        // console.log(parolaRegular);
+    }
+
+    for(let i = parolaScomposta.length; i >= 0; i--) {
+
+        parolaReverse.push(parolaScomposta[i]);
+        // console.log(parolaReverse);
+
+    }
+
+    if (parolaRegular.join("") === parolaReverse.join("")) {
+        console.log("la parola è palindroma");
+    } else {
+        console.log("la parola non è palindroma");
+    }
 }
 
-if (parolaDaSinistra.join("") === parolaDaDestra.join("")) {
-    console.log("la parola è plaindroma");
-} else {
-    console.log("la parola non è palindroma");
-}
-
-console.log(parolaDaDestra);
-console.log(parolaDaSinistra);
-
-
+console.log(palindroma(parolaUtente));
